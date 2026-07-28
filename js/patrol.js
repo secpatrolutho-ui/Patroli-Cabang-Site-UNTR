93737ecf1e59f9533b61663ede8817bb819afe23
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-28 16:15:49 +0700

    Update patrol.js

--- js/patrol.js
+++ js/patrol.js
@@ -78,6 +78,8 @@ async function loadCheckpoint(){
         checkpointData = result;
 
         tampilCheckpoint(result);
+        
+        mulaiGPS();
 
     }
 
@@ -108,6 +110,127 @@ function tampilCheckpoint(data){
 
     document.getElementById("checkpoint").innerText =
     data.checkpoint;
+
+}
+
+function mulaiGPS(){
+
+    if(!navigator.geolocation){
+
+        alert("Browser tidak mendukung GPS.");
+
+        return;
+
+    }
+
+    navigator.geolocation.watchPosition(
+
+        updateGPS,
+
+        gagalGPS,
+
+        {
+
+            enableHighAccuracy:true,
+
+            timeout:10000,
+
+            maximumAge:0
+
+        }
+
+    );
+
+}
+
+function updateGPS(position){
+
+    currentLat =
+    position.coords.latitude;
+
+    currentLng =
+    position.coords.longitude;
+
+    currentDistance =
+    hitungJarak(
+
+        currentLat,
+
+        currentLng,
+
+        Number(checkpointData.latitude),
+
+        Number(checkpointData.longitude)
+
+    );
+
+    updateStatusGPS();
+
+}
+
+function gagalGPS(error){
+
+    console.log(error);
+
+    document
+    .getElementById("gpsStatus")
+    .innerHTML="GPS Gagal";
+
+}
+
+function updateStatusGPS(){
+
+    document
+    .getElementById("distance")
+    .innerHTML =
+    currentDistance.toFixed(1)+" Meter";
+
+    const radius =
+    Number(checkpointData.radius);
+
+    const progress = currentDistance <= radius
+     ? 100
+     : Math.max(0, 100 - ((currentDistance - radius) / radius) * 100);
+   
+
+    );
+
+    document
+    .getElementById("progressBar")
+    .style.width =
+    progress+"%";
+
+    const status =
+    document
+    .getElementById("gpsStatus");
+
+    if(currentDistance<=radius){
+
+        status.innerHTML =
+        "✅ Dalam Radius";
+
+        status.className =
+        "status-success";
+
+        document
+        .getElementById("btnSubmit")
+        .disabled=false;
+
+    }
+
+    else{
+
+        status.innerHTML =
+        "❌ Di luar Radius";
+
+        status.className =
+        "status-danger";
+
+        document
+        .getElementById("btnSubmit")
+        .disabled=true;
+
+    }
 
 }
 
@@ -231,5 +354,35 @@ async function submitPatroli(){
         btn.disabled = false;
 
     }
+
+}
+
+function hitungJarak(lat1,lng1,lat2,lng2){
+
+    const R = 6371000;
+
+    const dLat =
+    (lat2-lat1)*Math.PI/180;
+
+    const dLng =
+    (lng2-lng1)*Math.PI/180;
+
+    const a =
+
+    Math.sin(dLat/2)*
+    Math.sin(dLat/2)+
+
+    Math.cos(lat1*Math.PI/180)*
+    Math.cos(lat2*Math.PI/180)*
+
+    Math.sin(dLng/2)*
+    Math.sin(dLng/2);
+
+    const c =
+    2*Math.atan2(
+    Math.sqrt(a),
+    Math.sqrt(1-a));
+
+    return R*c;
 
 }
