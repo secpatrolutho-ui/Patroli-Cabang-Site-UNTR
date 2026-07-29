1cca6c1deb6437d6cbe08607f0929b73663f9f9a
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-29 10:02:03 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,8 @@
-/* =====================================
-   CONFIG
-===================================== */
-
 const API =
 "https://script.google.com/macros/s/AKfycbwvMYlVBsxjef7-OVHqmiaUX0gecof7VEG2-JlMyp0sAeeJj_ANslssKDpXQJX-pbgY/exec";
 
-const GPS_OPTION={
-
+const GPS_OPTION = {
     enableHighAccuracy:true,
-
     timeout:10000,
-
     maximumAge:0
-
 };
-
-const DEFAULT_RADIUS=50;
