baa1f31028c3b8301767d588d5f8de8a65c429a1
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-29 11:12:53 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+console.log("APP BERHASIL DIMUAT");
+
+window.onload = function () {
+
+    console.log("window.onload");
+
+    checkLogin();
+
+    console.log("login OK");
+
+    loadCheckpoint();
+
+    console.log("checkpoint dipanggil");
+
+    initSubmit();
+
+    console.log("submit init");
+
+};
+
 window.onload=async function(){
 
     if(!checkLogin()) return;
