60dbb3359c50258af44c30112c41d0a01c36a8d5
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-29 11:35:57 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,39 @@
-const API = "https://script.google.com/macros/s/AKfycbwvMYlVBsxjef7-OVHqmiaUX0gecof7VEG2-JlMyp0sAeeJj_ANslssKDpXQJX-pbgY/exec";
+/* ===================================================
+   PATROLI DIGITAL CABANG & SITE
+   CONFIGURATION
+=================================================== */
+
+/* ========= APPS SCRIPT ========= */
+
+const API =
+"https://script.google.com/macros/s/AKfycbwvMYlVBsxjef7-OVHqmiaUX0gecof7VEG2-JlMyp0sAeeJj_ANslssKDpXQJX-pbgY/exec";
+
+/* ========= GPS ========= */
 
 const GPS_OPTION = {
+
     enableHighAccuracy: true,
+
     timeout: 10000,
+
     maximumAge: 0
+
 };
 
+/* ========= DEFAULT ========= */
+
 const DEFAULT_RADIUS = 50;
+
+/* ========= STATUS ========= */
+
+const STATUS = {
+
+    SUCCESS: "success",
+
+    ERROR: "error",
+
+    PENDING: "pending",
+
+    ACTIVE: "Active"
+
+};
