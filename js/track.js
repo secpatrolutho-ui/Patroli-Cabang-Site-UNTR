d5701025efe351437e8918871166cf7f7a82b91d
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-31 14:01:30 +0700

    Update track.js

--- js/track.js
+++ js/track.js
@@ -1,1 +1,74 @@
+/* ==========================================
+   SMART PATROL TRACK
+   Track Buffer Engine
+========================================== */
 
+let trackBuffer = [];
+
+/* ==========================================
+   RESET BUFFER
+========================================== */
+
+function resetTrackBuffer(){
+
+    trackBuffer = [];
+
+    console.log("TRACK BUFFER RESET");
+
+}
+
+/* ==========================================
+   ADD TRACK POINT
+========================================== */
+
+function addTrackPoint(point){
+
+    trackBuffer.push(point);
+
+    console.log(
+        "TRACK POINT :",
+        trackBuffer.length,
+        point
+    );
+
+}
+
+/* ==========================================
+   GET BUFFER
+========================================== */
+
+function getTrackBuffer(){
+
+    return trackBuffer;
+
+}
+
+/* ==========================================
+   TOTAL TRACK
+========================================== */
+
+function totalTrackPoint(){
+
+    return trackBuffer.length;
+
+}
+
+/* ==========================================
+   CLEAR BUFFER
+========================================== */
+
+function clearTrackBuffer(){
+
+    trackBuffer = [];
+
+}
+
+/* ==========================================
+   DEBUG
+========================================== */
+
+function showTrackBuffer(){
+
+    console.table(trackBuffer);
+
+}
