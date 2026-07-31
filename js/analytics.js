983b941b45752bcc241ae156fdb486eeecf259c2
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-31 16:55:25 +0700

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,154 @@
+/* ==========================================
+   SMART PATROL ANALYTICS
+   SPMS v1.0
+========================================== */
+
+/* ==========================================
+   TOTAL DISTANCE
+========================================== */
+
+function calculateTrackDistance(points){
+
+    if(!points || points.length < 2){
+
+        return 0;
+
+    }
+
+    let total = 0;
+
+    for(let i = 1; i < points.length; i++){
+
+        total += calculateDistance(
+
+            points[i-1].lat,
+            points[i-1].lng,
+
+            points[i].lat,
+            points[i].lng
+
+        );
+
+    }
+
+    return total;
+
+}
+
+/* ==========================================
+   DURATION
+========================================== */
+
+function calculateTrackDuration(points){
+
+    if(!points || points.length < 2){
+
+        return 0;
+
+    }
+
+    return (
+
+        points[points.length-1].timestamp -
+
+        points[0].timestamp
+
+    ) / 1000;
+
+}
+
+/* ==========================================
+   AVG SPEED
+========================================== */
+
+function calculateAverageSpeed(points){
+
+    const distance =
+    calculateTrackDistance(points);
+
+    const duration =
+    calculateTrackDuration(points);
+
+    if(duration <= 0){
+
+        return 0;
+
+    }
+
+    return distance / duration;
+
+}
+
+/* ==========================================
+   MAX SPEED
+========================================== */
+
+function calculateMaxSpeed(points){
+
+    let max = 0;
+
+    points.forEach(p=>{
+
+        if(p.speed > max){
+
+            max = p.speed;
+
+        }
+
+    });
+
+    return max;
+
+}
+
+/* ==========================================
+   SUMMARY
+========================================== */
+
+function getTrackSummary(){
+
+    const points = getTrackBuffer();
+
+    return{
+
+        patrolId:
+
+            localStorage.getItem("patrolId"),
+
+        totalPoint:
+
+            points.length,
+
+        totalDistance:
+
+            calculateTrackDistance(points),
+
+        duration:
+
+            calculateTrackDuration(points),
+
+        averageSpeed:
+
+            calculateAverageSpeed(points),
+
+        maxSpeed:
+
+            calculateMaxSpeed(points)
+
+    };
+
+}
+
+/* ==========================================
+   DEBUG
+========================================== */
+
+function previewSummary(){
+
+    console.table(
+
+        getTrackSummary()
+
+    );
+
+}
