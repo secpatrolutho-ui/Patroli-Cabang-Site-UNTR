cb9b170b68e24c8f05f558d85b8d513addc9e792
secpatrolutho-ui <secpatrol.utho@gmail.com>
2026-07-31 16:18:55 +0700

    Update gps.js

--- js/gps.js
+++ js/gps.js
@@ -64,6 +64,8 @@ function updateGPS(position){
 
 if(isPatrolActive()){
 
+    const now = new Date();
+   
     addTrackPoint({
 
         patrolId:
@@ -84,6 +86,14 @@ if(isPatrolActive()){
 
         timestamp:
         Date.now()
+
+        deviceTime:
+            now.getFullYear() + "-" +
+            String(now.getMonth()+1).padStart(2,"0") + "-" +
+            String(now.getDate()).padStart(2,"0") + " " +
+            String(now.getHours()).padStart(2,"0") + ":" +
+            String(now.getMinutes()).padStart(2,"0") + ":" +
+            String(now.getSeconds()).padStart(2,"0")
 
     });
 
